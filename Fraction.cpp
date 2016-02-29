58017e2f4a58b529931c5a63fd894e600cd10498
Preshen1702 <214532872@stu.ukzn.ac.za>
2016-02-29 09:27:20 +0200

    Correcting Fraction

--- Fraction.cpp
+++ Fraction.cpp
@@ -4,79 +4,62 @@ using namespace std;
 class fraction{
 public:
 	fraction();
-	void setFraction(float a, float b, float c, float d);
-	float getFraction1();
-	float getFraction2();
-	float add();
-	float sub();
-	float multiply();
-	float divide();
+	void setFraction(int a, int b);
+	int getNum();
+	int getDen();
+	int add(fraction a);
+	int sub(fraction a);
+	int multiply(fraction a);
+	int divide(fraction a);
 	void input();
 	void print();
 private:
-	float num, num2;
-	float den, den2;
+	int num, den;
 };
 int main()
 {
-	fraction objFrac;
-	objFrac.input();
-	objFrac.add();
-	objFrac.sub();
-	objFrac.multiply();
-	objFrac.divide();
-	objFrac.print();
+	fraction obj1,obj2;
+
 }
 fraction::fraction()
 {
 	num = 0;
 	den = 0;
-	num2 = 0;
-	den2 = 0;
 }
-float fraction::getFraction1()
+int fraction::getNum()
 {
-	return num / den;
+	return num ;
 }
-float fraction::getFraction2()
+int fraction::getDen()
 {
-	return num2 / den2;
+	return den;
 }
-void fraction::setFraction(float a, float b, float c, float d)
+void fraction::setFraction(int a, int b)
 {
 	num = a;
 	den = b;
-	num2 = c;
-	den2 = d;
 }
-float fraction::add()
+int fraction::add(fraction a)
 {
-	return getFraction1() + getFraction2();
+	return a.getNum()+num;
 }
-float fraction::sub()
+int fraction::sub(fraction a)
 {
-	return getFraction1() - getFraction2();
+	return a.getNum() - num;
 }
-float fraction::multiply()
+int fraction::multiply(fraction a)
 {
-	return getFraction1() * getFraction2();
+	return a.getNum() * num;
 }
-float fraction::divide()
+int fraction::divide(fraction a)
 {
-	return getFraction1() / getFraction2();
+	return a.getNum() / num;
 }
 
-void fraction::print()
-{
-	cout << "Addition : " << add() << endl;
-	cout << "Subtraction :" << sub() << endl;
-	cout << "Multiplication : " << multiply() << endl;
-	cout << "Division : " << divide() << endl;
-}
 void fraction::input()
 {
-	float a,s,d,f;
+	float a,s;
 	cout << "Ennter a numerator and denomenator" << endl;
-	cin >> a >> s >> d >> f;
-	setFraction(a, s, d, f);
+	cin >> a >> s;
+	setFraction(a, s);
 }
