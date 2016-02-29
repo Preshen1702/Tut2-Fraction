b15e399fd69e785b40830bff73eaf04ca750cd06
Preshen1702 <214532872@stu.ukzn.ac.za>
2016-02-29 11:21:49 +0200

    Correction to Tut 2 (Complete)

--- Fraction.cpp
+++ Fraction.cpp
@@ -3,17 +3,53 @@ using namespace std;
 
 class fraction{
 public:
-	fraction(int n1, int n2);
-	fraction();
-	void setFraction(int a, int b);
-	int getNum();
-	int getDen();
-	fraction add(fraction a);
-	int sub(fraction a);
-	int multiply(fraction a);
-	int divide(fraction a);
-	void input();
-	void print();
+	fraction()
+	{
+		num = 0;
+		den = 0;
+	}
+	fraction(int n1, int n2)
+	{
+		num = n1;
+		den = n2;
+	}
+	int getNum()
+	{
+		return num;
+	}
+	int getDen()
+	{
+		return den;
+	}
+	void setFraction(int a, int b)
+	{
+		num = a;
+		den = b;
+	}
+	fraction add(fraction a)
+	{
+		fraction ret;
+		ret.setFraction((a.getNum() + num), (a.getDen() + den));
+		return ret;
+	}
+	fraction sub(fraction a)
+	{
+		fraction ret;
+		ret.setFraction((a.getNum() - num), (a.getDen() - den));
+		return ret;
+	}
+	fraction multi(fraction a)
+	{
+		fraction ret;
+		ret.setFraction((a.getNum() * num), (a.getDen() * den));
+		return ret;
+	}
+	fraction divide(fraction a)
+	{
+		fraction ret;
+		ret.setFraction((a.getNum() / num), (a.getDen() / den));
+		return ret;
+	}
 private:
 	int num, den;
 };
@@ -23,60 +59,18 @@ int main()
 	cout << "Enter a numerator and denomenator" << endl;
 	cin >> n1 >> n2;
 	fraction obj = fraction(n1,n2);
-	fraction temp;
+	fraction temp1, temp2, temp3, temp4;
 
 	int no1, no2;
 	cout << "Enter a numerator and denomenator to add" << endl;
 	cin >> no1 >> no2;
 	fraction obj1 = fraction(no1, no2);
-	temp = obj.add(obj1);
-	cout << "The addition is " << temp.getNum() << "/" << temp.getDen() << endl;
-}
-fraction::fraction(int n1, int n2)
-{
-	num = n1;
-	den = n2;
-}
-fraction::fraction()
-{
-
-}
-int fraction::getNum()
-{
-	return num ;
-}
-int fraction::getDen()
-{
-	return den;
-}
-void fraction::setFraction(int a, int b)
-{
-	num = a;
-	den = b;
-}
-fraction fraction::add(fraction a)
-{
-	fraction ret;
-	ret.setFraction((a.getNum()+num),(a.getDen()+den));
-	return ret;
-}
-int fraction::sub(fraction a)
-{
-	return a.getNum() - num;
-}
-int fraction::multiply(fraction a)
-{
-	return a.getNum() * num;
-}
-int fraction::divide(fraction a)
-{
-	return a.getNum() / num;
-}
-
-void fraction::input()
-{
-	float a,s;
-	cout << "Ennter a numerator and denomenator" << endl;
-	cin >> a >> s;
-	setFraction(a, s);
+	temp1 = obj.add(obj1);
+	temp2 = obj.sub(obj1);
+	temp3 = obj.multi(obj1);
+	temp4 = obj.divide(obj1);
+	cout << obj1.getNum() << "/" << obj1.getDen() << "+" << obj.getNum() << "/" << obj.getDen() <<" = "<< temp1.getNum() << "/" << temp1.getDen() << endl;
+	cout << obj1.getNum() << "/" << obj1.getDen() << "-" << obj.getNum() << "/" << obj.getDen() << " = " << temp2.getNum() << "/" << temp2.getDen() << endl;
+	cout << obj1.getNum() << "/" << obj1.getDen() << "*" << obj.getNum() << "/" << obj.getDen() << " = " << temp3.getNum() << "/" << temp3.getDen() << endl;
+	cout << obj1.getNum() << "/" << obj1.getDen() << "/" << obj.getNum() << "/" << obj.getDen() << " = " << temp4.getNum() << "/" << temp4.getDen() << endl;
 }
